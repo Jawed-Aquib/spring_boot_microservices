7a00875e6d1b89102db5d807523332e3d6108f63
AquibJawed <jawedaquib2010@gmail.com>
2021-12-26 16:19:02 +0530

    service discovery done by eureka server and client

--- spring-cloud-config-server/src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -3,6 +3,7 @@ package com.in28minutes.microservices.springcloudconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableConfigServer
 @SpringBootApplication
